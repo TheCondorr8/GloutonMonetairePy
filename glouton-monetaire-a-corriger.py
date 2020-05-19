baf81842410c0db3ccdbe3e9e18340e086cc0d41
TheCondorr8 <63144152+TheCondorr8@users.noreply.github.com>
2020-05-19 13:45:12 +0200

    Update glouton-monetaire-a-corriger.py

--- glouton-monetaire-a-corriger.py
+++ glouton-monetaire-a-corriger.py
@@ -3,29 +3,37 @@
 # NF.NSI
 
 # montant de la monnaie donnée
-montant = 1.65
+montant = float(input())
 
 # valeur des pieces disponibles en euro trié dans l'ordre décroissant
 pieces = [ 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 ]
+billets = [500, 200, 100, 50, 20, 10, 5]
 
 ## exemple de cas non optimal
 ## montant = 21
 ## pieces = [ 18, 7, 1 ]
 
-def Monnaie(somme, ListeMontant) :
+def Monnaie(somme, ListeMontantPiece, ListeMontantBillet) :
 
     # tableau de nombre de piece max a rendre selon le tableau de pieces
-    for ListeNbPiece[-1] in ListeMontant :
+    ListeNbPieces = [-1 for k in ListeMontantPiece]
+    ListeNbBillets = [-1 for k in ListeMontantBillet]
 
-        # parcours de la liste des pieces
-        for k in range(len(ListeMontant)) :
+    for k in range(len(ListeMontantBillet)) :
 
-            # recupere le nombre de piece selon le quotient (entier //)
-            NbPieces[k] = somme // ListeMontant[y]
+        ListeNbBillets[k] = somme // ListeMontantBillet[k]
 
-            # somme restante a deduire du montant
-            somme = arrondi(somme % ListeMontant['k'], 2)
+        somme = round(somme % ListeMontantBillet[k], 2)
 
-    return somme, ListeNbPieces
+    # parcours de la liste des pieces
+    for k in range(len(ListeMontantPiece)) :
 
-print(Monnaie(montant, piece))
+        # recupere le nombre de piece selon le quotient (entier //)
+        ListeNbPieces[k] = somme // ListeMontantPiece[k]
+
+        # somme restante a deduire du montant
+        somme = round(somme % ListeMontantPiece[k], 2)
+
+    return somme, ListeNbBillets, ListeNbPieces
+
+print(montant, Monnaie(montant, pieces, billets))
